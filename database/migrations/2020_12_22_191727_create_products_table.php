b0adfcd94c8495a780995462906c996cbd71cba3
Burak Vanli <bsvanli@gmail.com>
2020-12-23 00:41:20 +0300

    Migration prepared

---
+++ database/migrations/2020_12_22_191727_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class CreateProductsTable.
+ */
+class CreateProductsTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('products', function(Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->decimal('price');
+            $table->tinyInteger('status');
+            $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('products');
+	}
+}
